a81a78a44e9107254fa1deeb78d8cd1a70cce3c6
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-03 14:31:06 +0100

    checkpoint timining

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 // Traccia
 // Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
 // Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+const secondiEl = document.getElementById("secondi");
+const minutiEl = document.getElementById("minuti");
+const oraEl = document.getElementById("ora");
 
+const clock = setInterval(timeAttuale, 1000);
+function timeAttuale() {
+    let now = new Date();
+    const secondi = now.getSeconds();
+    const minuti = now.getMinutes();
+    const ora = now.getHours();
+
+    console.log("mancano: " + now.getTime("2023-02-04 09:30")); //è corretto cosi?
+    clearInterval(clock);
+}
