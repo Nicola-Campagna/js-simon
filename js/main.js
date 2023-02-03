5c7d15666610da420200aab5b4eba472ca1f93eb
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-03 14:04:42 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Traccia
+// Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
+// Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+
